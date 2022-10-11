dcd7976fc2627448d83a83fdb45b518f4df1a34a
KB <kj.br@protonmail.com>
2022-10-10 21:47:30 -0500

    Update Styling

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,7 +2,5 @@ const q = (query) => document.querySelector(query)
 const qA = (query) => [...document.querySelectorAll(query)]
 const create = (query) => document.createElement(query)
 
-
-
 export {q, qA,create}
 
