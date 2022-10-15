f159b389846eebf7490b4bddd139f73c2c6505dc
KB <kj.br@protonmail.com>
2022-10-15 11:59:07 -0500

    Build Modal

---
+++ src/js/buildModal_HTML.js
@@ -0,0 +1,28 @@
+export const buildModal = () =>{
+    const modal = 
+    `
+    <div class="modal active">
+        <div class="modalTitle">
+            <div>Create new: </div>
+            <div class="newType active" id="typeTask">Task</div>
+            <div class="newType" id="typeProject">Project</div>
+        </div>
+        <div class="modalPrioritySelector">
+            <div>Priority:</div>
+            <div class='modalPriority' id="priorityLow">Low</div>    
+            <div class='modalPriority' id="priorityMedium">Medium</div>    
+            <div class='modalPriority' id="priorityHigh">High</div>    
+        </div>
+        <div class="modalDueDate">
+            <div>Due: </div>
+            <div> calendar</div>
+        </div>
+        <div class="modalDetails">
+            <div>Details:</div>
+            <textarea name="modalDetails" id="modalDetails" cols="49" rows="8"></textarea>
+        </div>
+        <button type="submit" id="modalSubmit" class="">Create</button>
+    </div>
+    `
+    return modal
+}
